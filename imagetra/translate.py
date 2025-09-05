830eb0222b0effb2642350c0f41b68f89c67580e
hour <hour@users.noreply.github.com>
2025-09-05 12:30:25 +0900

    live in local machine with imagetra_translate

--- imagetra/translate.py
+++ imagetra/translate.py
@@ -68,6 +68,47 @@ def run_imgs(args, pipeline, filter):
     logfile.print(f'Execute time:\n{timer.format()}')
     logfile.close()
 
+def run_real_time(args, pipeline, filter):
+    import cv2, time
+    from imagetra.common.media import Image
+    cap = cv2.VideoCapture(args.input)
+
+    def _gen():
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                raise ValueError("Error: Can't receive frame (stream end?). Exiting ...")
+            frame = Image(frame, channel_first=False)
+            yield frame
+    
+    # preprocess cap
+    if args.fps is not None:
+        cap.set(cv2.CAP_PROP_FPS, args.fps)
+    
+    if args.scale is not None:
+        width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+        height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        new_width = int(width * args.scale)
+        new_height = int(height * new_width / width)
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, new_width)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, new_height)
+
+    wkargs = {'tracker_type': args.tracker_type} if args.tracking else {}
+
+    for result in pipeline(_gen(), fn_filter=filter.filter, **wkargs):
+        if args.verbose:
+            out_img = result.img.draw_bboxs(result.bboxs)
+        else:
+            out_img = result.img
+        
+        cv2.imshow('Camera Feed', out_img.image)
+        # Exit loop if 'q' key is pressed
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
 def main(argv=None):
     from argparse import ArgumentParser
     parser = ArgumentParser()
@@ -78,9 +119,16 @@ def main(argv=None):
     parser.add_argument('--tracking', action='store_true')
     parser.add_argument('--tracker_type', default=boxmot.DEFAULT_TRACKER_TYPE)
     parser.add_argument('--verbose', '-v', action='store_true')
+    parser.add_argument('--scale', type=float)
+    parser.add_argument('--fps', type=int)
     args = parser.parse_args(argv)
 
-    filetype = mimetypes.guess_type(args.input)[0]
+    try:
+        args.input = int(args.input)
+        filetype = 'camera'
+    except:
+        filetype = mimetypes.guess_type(args.input)[0]
+
     config = Config.load_yaml(args.config)
     
     filter = BboxFilter(
@@ -93,13 +141,15 @@ def main(argv=None):
     pipeline_name = 'vdo2vdo' if args.tracking else 'img2img'
     pipeline = build_pipeline(pipeline_name, config, logfile=args.logfile)
 
-    if config.common_greedy:
+    if config.common_greedy or 'camera' in filetype:
         pipeline = pipeline.iter
 
     if 'video' in filetype:
         run_video(args, pipeline, filter)
     elif 'image' in filetype:
         run_imgs(args, pipeline, filter)
+    elif 'camera' in filetype:
+        run_real_time(args, pipeline, filter)
     else:
         raise ValueError(f'Unknown type {filetype}')
 
